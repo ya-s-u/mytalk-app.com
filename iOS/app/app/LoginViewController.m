b5687b1d5eea997f7e4eb0482719bae7fc253a7c
masanari575 <masanarikamoshita@r00886538309a.wll.prv.nitech.ac.jp>
2014-07-01 16:05:29 +0900

    loginViewController

---
+++ iOS/app/app/LoginViewController.m
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.m
+//  app
+//
+//  Created by MasanariKamoshita on 2014/07/01.
+//  Copyright (c) 2014年 MasanariKamoshita. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "AccountSet.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+@synthesize account;
+
+@end
